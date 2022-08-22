376683e6fecfc6a17ba14adedff9286649c66b0b
tang_yue1129@163.com <tangyue5@tal.com>
2022-08-22 20:03:37 +0800

    feat: init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      // Allows for the parsing of JSX
+      jsx: true
+    }
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended', 
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    // 项目初始化vite-env.d.ts会报的错
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        "extendDefaults": true,
+        "types": {
+          "{}": false
+        }
+      }
+    ],
+    // 可以使用any类型
+    "@typescript-eslint/no-explicit-any": 'off'
+  }
+}
